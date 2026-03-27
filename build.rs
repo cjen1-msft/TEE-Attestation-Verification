866c1c69f85ebe232e64c9e99929a3c0286cfa03
cjen1-msft <chrisjensen@microsoft.com>
2026-03-27 17:26:43 +0000

    Sync and async verifier (#25)

---
+++ build.rs
@@ -0,0 +1,22 @@
+fn main() {
+    println!("cargo:rustc-check-cfg=cfg(sync_crypto)");
+    println!("cargo:rustc-check-cfg=cfg(async_crypto)");
+    println!("cargo:rustc-check-cfg=cfg(crypto_backend, values(\"crypto_openssl\", \"crypto_pure_rust\"))");
+
+    let has_openssl = std::env::var_os("CARGO_FEATURE_CRYPTO_OPENSSL").is_some();
+    let has_pure_rust = std::env::var_os("CARGO_FEATURE_CRYPTO_PURE_RUST").is_some();
+
+    let crypto_backend = if has_pure_rust {
+        Some("crypto_pure_rust")
+    } else if has_openssl {
+        Some("crypto_openssl")
+    } else {
+        None
+    };
+
+    if let Some(crypto_backend) = crypto_backend {
+        println!("cargo:rustc-cfg=sync_crypto");
+        println!("cargo:rustc-cfg=async_crypto");
+        println!("cargo:rustc-cfg=crypto_backend=\"{crypto_backend}\"");
+    }
+}
